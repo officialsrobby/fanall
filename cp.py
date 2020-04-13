f40f3ee20abc5b0db14bf771441de0e4d52bb64e
Robby Pratama W <58054589+officialsrobby@users.noreply.github.com>
2020-04-13 16:10:16 +0700

    Update and rename fl.py to cp.py

--- cp.py
+++ cp.py
@@ -7,7 +7,7 @@ a = list(range(210900010,220000000))
 i=1
 for tokens in a:
 	try:
-		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token 29b0534f029ff4614cffbd466a5c1b2327f65f72'}
+		headers={'User-Agent':'Mozilla/5.0','Authorization':'Token 510b152118e524377ef293c3576d26511c2bddbb'}
 		response = requests.post('https://id-api.spooncast.net/users/'+str(tokens)+'/follow/',headers=headers,timeout=1)
 		print(response)
 		print(i)
